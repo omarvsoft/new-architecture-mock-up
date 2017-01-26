337bc50c1c4ad248b69e2f4c7398dd9ad9266db3
Erick Vargas <omarvsoft@gmail.com>
2017-01-26 16:16:16 -0400

    Configuring cluster and clients for redis

--- client-side/web-modules/authentication-web/src/main/java/com/evertec/cibp/web/authentication/configuration/CacheConfig.java
+++ client-side/web-modules/authentication-web/src/main/java/com/evertec/cibp/web/authentication/configuration/CacheConfig.java
@@ -1,67 +1,67 @@
 package com.evertec.cibp.web.authentication.configuration;
 
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;;
-
-//redis/clients/jedis/JedisPoolConfig
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching
 public class CacheConfig extends CachingConfigurerSupport {
 
-  @Bean
-  public JedisConnectionFactory redisConnectionFactory() {
-    JedisConnectionFactory redisConnectionFactory = new JedisConnectionFactory();
+	/**
+	 * Type safe representation of application.properties
+	 */
+	@Autowired
+	ClusterConfigurationProperties clusterProperties;
 
-    // Defaults
-    redisConnectionFactory.setHostName("127.0.0.1");
-    redisConnectionFactory.setPort(7777);
-    return redisConnectionFactory;
-  }
+	@Bean
+	public JedisConnectionFactory redisConnectionFactory() {
+		return new JedisConnectionFactory(new RedisClusterConfiguration(clusterProperties.getNodes()));
+	}
 
-  @Bean
-  public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory cf) {
-    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
-    redisTemplate.setConnectionFactory(cf);
-    
-    redisTemplate.setKeySerializer(new StringRedisSerializer());
-    redisTemplate.setHashValueSerializer( new GenericToStringSerializer< Object >( Object.class ) );
-    //redisTemplate.setValueSerializer( new GenericToStringSerializer< Object >( Object.class ) );
-    redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-    
-    return redisTemplate;
-  }
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory cf) {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+		redisTemplate.setConnectionFactory(cf);
 
-  @Bean
-  public CacheManager cacheManager(RedisTemplate<String,Object> redisTemplate) {
-    RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setHashValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 
-    // Number of seconds before expiration. Defaults to unlimited (0)
-    cacheManager.setDefaultExpiration(300);
-    return cacheManager;
-  }
-  
-  @Bean
-  public GenericObjectPoolConfig jedisPoolConfig() {
-	  
-	  GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
-	  genericObjectPoolConfig.setTestWhileIdle(true);
-	  genericObjectPoolConfig.setMinEvictableIdleTimeMillis(60000);
-	  genericObjectPoolConfig.setTimeBetweenEvictionRunsMillis(30000);
-	  genericObjectPoolConfig.setNumTestsPerEvictionRun(-1);
-	  
-	  return genericObjectPoolConfig;
-	  
-  }
+		return redisTemplate;
+	}
+
+	@Bean
+	public CacheManager cacheManager(RedisTemplate<String, Object> redisTemplate) {
+		RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+
+		// Number of seconds before expiration. Defaults to unlimited (0)
+		cacheManager.setDefaultExpiration(300);
+		return cacheManager;
+	}
+
+	@Bean
+	public GenericObjectPoolConfig jedisPoolConfig() {
+
+		GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
+		genericObjectPoolConfig.setTestWhileIdle(true);
+		genericObjectPoolConfig.setMinEvictableIdleTimeMillis(60000);
+		genericObjectPoolConfig.setTimeBetweenEvictionRunsMillis(30000);
+		genericObjectPoolConfig.setNumTestsPerEvictionRun(-1);
+
+		return genericObjectPoolConfig;
+
+	}
 }
--- client-side/web-modules/customer-web/src/main/java/com/evertec/cibp/web/customer/configuration/CacheConfig.java
+++ client-side/web-modules/customer-web/src/main/java/com/evertec/cibp/web/customer/configuration/CacheConfig.java
@@ -1,12 +1,14 @@
 package com.evertec.cibp.web.customer.configuration;
 
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -17,49 +19,51 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 @EnableCaching
 public class CacheConfig extends CachingConfigurerSupport {
+	
+	/**
+	 * Type safe representation of application.properties
+	 */
+	@Autowired
+	ClusterConfigurationProperties clusterProperties;
 
-  @Bean
-  public JedisConnectionFactory redisConnectionFactory() {
-    JedisConnectionFactory redisConnectionFactory = new JedisConnectionFactory();
+	@Bean
+	public JedisConnectionFactory redisConnectionFactory() {
+		return new JedisConnectionFactory(new RedisClusterConfiguration(clusterProperties.getNodes()));
+	}
 
-    // Defaults
-    redisConnectionFactory.setHostName("127.0.0.1");
-    redisConnectionFactory.setPort(7777);
-    return redisConnectionFactory;
-  }
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory cf) {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+		redisTemplate.setConnectionFactory(cf);
 
-  @Bean
-  public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory cf) {
-    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
-    redisTemplate.setConnectionFactory(cf);
-    
-    redisTemplate.setKeySerializer(new StringRedisSerializer());
-    redisTemplate.setHashValueSerializer( new GenericToStringSerializer< Object >( Object.class ) );
-    //redisTemplate.setValueSerializer( new GenericToStringSerializer< Object >( Object.class ) );
-    redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-    
-    return redisTemplate;
-  }
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setHashValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+		// redisTemplate.setValueSerializer( new GenericToStringSerializer<
+		// Object >( Object.class ) );
+		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 
-  @Bean
-  public CacheManager cacheManager(RedisTemplate<String,Object> redisTemplate) {
-    RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+		return redisTemplate;
+	}
 
-    // Number of seconds before expiration. Defaults to unlimited (0)
-    cacheManager.setDefaultExpiration(300);
-    return cacheManager;
-  }
-  
-  @Bean
-  public GenericObjectPoolConfig jedisPoolConfig() {
-	  
-	  GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
-	  genericObjectPoolConfig.setTestWhileIdle(true);
-	  genericObjectPoolConfig.setMinEvictableIdleTimeMillis(60000);
-	  genericObjectPoolConfig.setTimeBetweenEvictionRunsMillis(30000);
-	  genericObjectPoolConfig.setNumTestsPerEvictionRun(-1);
-	  
-	  return genericObjectPoolConfig;
-	  
-  }
+	@Bean
+	public CacheManager cacheManager(RedisTemplate<String, Object> redisTemplate) {
+		RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+
+		// Number of seconds before expiration. Defaults to unlimited (0)
+		cacheManager.setDefaultExpiration(300);
+		return cacheManager;
+	}
+
+	@Bean
+	public GenericObjectPoolConfig jedisPoolConfig() {
+
+		GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
+		genericObjectPoolConfig.setTestWhileIdle(true);
+		genericObjectPoolConfig.setMinEvictableIdleTimeMillis(60000);
+		genericObjectPoolConfig.setTimeBetweenEvictionRunsMillis(30000);
+		genericObjectPoolConfig.setNumTestsPerEvictionRun(-1);
+
+		return genericObjectPoolConfig;
+
+	}
 }
